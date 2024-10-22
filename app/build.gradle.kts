bae76f0e045ccc8a7f0b49fc678bd03caa6986a7
mlopez <monicalopezoliveros@gmail.com>
2024-10-22 09:48:32 -0500

    Added Moshi libraries and dependencies to libs.versions.toml and build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // moshi
+    implementation(libs.moshi.kotlin)
+    implementation(libs.converter.moshi)
 }
